e89fbe4204505b4ff91a33f6727b36bfd5a2e849
FBIcome <kdckon@gmail.com>
2023-09-09 14:53:18 +0900

    20230909
    와피스

--- edu/urls.py
+++ edu/urls.py
@@ -5,4 +5,6 @@ app_name = 'edu'
 
 urlpatterns = [
     path('', views.Index.as_view(), name= 'index'),
+
+    path('tag', views.TagStudy.as_view(), name='tag_study'),
 ]
--- edu/views.py
+++ edu/views.py
@@ -8,3 +8,8 @@ class Index(View):
     def get(self, request):
         return render(request, self.template_name)      
 
+class TagStudy(View):
+    template_name= 'tag_study.html'
+
+    def get(self, request):
+        return render(request, self.template_name)      
